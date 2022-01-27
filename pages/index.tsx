17c6e740a8c63dc6e133bee3494d70f4b8b9dd2d
Laughaholicgeek <umangsharm@gmail.com>
2022-01-27 17:01:46 +0530

    Added navbar in app.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-    Homepage
-    <p>
-      
-      <a href = "/">  Home  |</a>
-      <a rel="noreferrer" href = "/about"  >|  About Us  |</a> 
-      <a rel="noreferrer" href = "/activities"  >|  Activities  |</a> 
-      <a rel="noreferrer" href = "/ecotourism"  >|  Ecotourism  |</a>
-      <a rel="noreferrer" href = "/contact"  >|  Contact Us  </a> 
-      
-      
-      
-    </p>
+   
     </div>
   )
 }
---
+++ src/navbar.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+
+const Navbar: NextPage = () => {
+  
+  return (
+    <div>
+     
+    <p>
+      
+      <a href = "/">  Home  |</a>
+      <a rel="noreferrer" href = "/about"  >|  About Us  |</a> 
+      <a rel="noreferrer" href = "/activities"  >|  Activities  |</a> 
+      <a rel="noreferrer" href = "/ecotourism"  >|  Ecotourism  |</a>
+      <a rel="noreferrer" href = "/contact"  >|  Contact Us  </a> 
+      
+      
+      
+    </p>
+    </div>
+  )
+}
+
+export default Navbar
